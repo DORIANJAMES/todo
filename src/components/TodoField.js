590e4e818858a8af1ddde5f38c38ddce76de4a3d
Dorian JAMES <dorianthelycan@gmail.com>
2023-09-21 15:57:23 +0300

    Input focused bug caused by button clicking is fixed.

--- src/components/TodoField.js
+++ src/components/TodoField.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {v4 as uuidv4} from "uuid";
 
 function TodoField({addTodo}) {
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState('');
+    const inputRef = useRef();
 const addWithClick = () => {
         addTodo(
             {
@@ -11,12 +12,13 @@ const addWithClick = () => {
                 status: false,
             });
             setValue('');
+            inputRef.current.focus();
     }
     return (
         <>
             <div className="todoField">
                 <input
-
+                    ref={inputRef}
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
                     type="text"
